3ca6a746f12d4565f92db7506e4615f69c07407e
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2020-10-15 08:19:21 -0700

    completed task 2

--- index.js
+++ index.js
@@ -74,7 +74,28 @@ class Person {
 */
 
 class Car {
-
+    constructor(model, milesPerGallon) {
+        this.model = model;
+        this.milesPerGallon = milesPerGallon;
+        this.tank = 0;
+        this.odometer = 0;
+    }
+    fill(gallons) {
+        this.tank += gallons;
+    }
+    drive(distance) {
+        let availableDistance = this.tank * this.milesPerGallon;
+        if (distance > availableDistance) {
+            distance = availableDistance;
+        }
+        this.odometer += distance;
+        this.tank -= (distance / this.milesPerGallon);
+        if (this.tank <= 0) {
+            return `I ran out of fuel at ${this.odometer} miles!`;
+        } else {
+            return "";
+        }
+    }
 }
 
 /*
