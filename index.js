300f04e654913e0b89ae6ba946d9c1f31cb34e43
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2020-10-15 08:54:17 -0700

    updated task 3 and 4

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ class Lambdasian {
         this.location = object.location;
     }
     speak() {
-        return `Hello my name is ${name}, I am from ${location}`;
+        return `Hello my name is ${this.name}, I am from ${this.location}`;
     }
 }
 
@@ -136,8 +136,19 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian {
+    constructor(object) {
+        super(object);
+        this.specialty = object.specialty;
+        this.favLanguage = object.favLanguage;
+        this.catchPhrase = object.catchPhrase;
+    }
+    demo() {
+        return
+    }
+    grade() {
+        return `{student.name} receives a perfect score on {subject}`
+    }
 }
 
 /*
